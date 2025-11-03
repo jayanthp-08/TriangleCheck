0788628a5c927f5a8403994ab1b3fd3000c43c8b
jayanthp-08 <jayanthp0809@gmail.com>
2025-11-03 16:22:45 +0530

    Update RightAngleTriangleTest.java

--- src/RightAngleTriangleTest.java
+++ src/RightAngleTriangleTest.java
@@ -1,6 +1,5 @@
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import static org.junit.Assert.*; 
 public class RightAngleTriangleTest {
     @Test
     public void testValidTriangle1() {
